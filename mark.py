f122b3ea94b79facb1293c1a45df42cab9acfbca
pratiksha1112 <pratiksha1113@gmail.com>
2026-05-31 17:33:42 +0530

    initial commit

---
+++ mark.py
@@ -0,0 +1,5 @@
+m1 = int(input("Enter the marks of subject 1: ")) 
+m2 = int(input("Enter the marks of subject 2: ")) 
+m3 = int(input("Enter the marks of subject 3: ")) 
+total = m1 + m2 + m3
+print("Total marks: ", total)
